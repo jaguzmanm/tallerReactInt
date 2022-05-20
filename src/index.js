ff3fec30f735d49afcc4612c57b4220e84ebcece
John Guzmán <60189807+jaguzmanm@users.noreply.github.com>
2022-05-19 23:07:43 -0500

    Taller terminado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider } from "react-intl";
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const locale = navigator.language;
+let lang;
+let theme;
+if (locale.startsWith("es")) {
+  lang = localeEsMessages;
+  theme = "light";
+} else {
+  lang = localeEnMessages;
+  theme = "dark";
+}
+
+ReactDOM.render(
+  <IntlProvider locale={locale} messages={lang}>
+    <JobsList theme={theme} />
+  </IntlProvider>,
+  document.getElementById("root")
+);
